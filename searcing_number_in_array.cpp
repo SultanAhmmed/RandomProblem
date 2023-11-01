b18861b83796670f7203b5e01cc039cb681c69e6
Sultan Ahmmed <68941578+SultanAhmmed@users.noreply.github.com>
2023-11-01 10:58:43 +0600

    Delete searcing_number_in_array.cpp

--- searcing_number_in_array.cpp
+++
@@ -1,8 +0,0 @@
-int search(vector<int> &nums, int target) {
-    int n = nums.size();
-    int pos = -1;
-    pos = lower_bound(nums.begin(),nums.end(), target) - nums.begin();
-    
-    if( nums[pos] == target ) return pos;
-    else return -1;
-}
